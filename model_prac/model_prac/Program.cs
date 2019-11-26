9bbdbc0706fc5f80bf89e92e406a02a3d69333e0
Harish S G <42273915+sgharish@users.noreply.github.com>
2019-11-26 19:54:05 +0100

    Add files via upload

---
+++ model_prac/model_prac/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model_prac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello ppl I will fight");
+            Console.ReadLine();// keep the termial 
+        }
+    }
+}
