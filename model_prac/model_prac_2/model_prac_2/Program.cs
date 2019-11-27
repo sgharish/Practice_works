90e83247ed92349967eda70a7f56e97803ff7cbc
Harish S G <42273915+sgharish@users.noreply.github.com>
2019-11-27 08:33:07 +0100

    Add files via upload

---
+++ model_prac/model_prac_2/model_prac_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model_prac_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string charName = "Harry";
+            int charAge;
+            charAge = 21;
+            Console.WriteLine("there was this guy named " + charName);
+            Console.WriteLine("he was "+ charAge +"  years old");
+            charName = "H gopal";
+            Console.WriteLine(""+ charName +" loved himself so much ");
+            Console.WriteLine("He is a not brainy,but a he fights for his victory");
+
+            Console.ReadLine();
+        }
+    }
+}
