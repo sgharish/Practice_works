7e3c6c513fc66e01048b95d72ff36f84c48ecb98
Harish S G <42273915+sgharish@users.noreply.github.com>
2019-11-27 14:00:44 +0100

    Harry PC
    This has a set of code that teaches about some of the method operations available in the C# programming language.
    Note: these codes are uploaded immediately after learning the concept

---
+++ model_prac/model_prac3/model_prac3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model_prac3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            num1 = 70;
+            num2 = 40;
+            float num3;
+            Console.WriteLine(num1*num2);
+            Console.WriteLine(Math.Min(num1,num2));
+            Console.WriteLine(Math.Log10(num1));
+            Console.WriteLine(Math.PI);
+          
+
+    
+            Console.WriteLine(Math.Sqrt(9));
+            Console.ReadLine();
+        }
+    }
+}
