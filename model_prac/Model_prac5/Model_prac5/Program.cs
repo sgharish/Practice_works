05ae73fde9a1297ca422acf4f085239eeb6f0ed3
Harish S G <42273915+sgharish@users.noreply.github.com>
2019-11-28 18:44:44 +0100

    Harry Laptop
    Learning code on Array how to manage and change elements in the code.

---
+++ model_prac/Model_prac5/Model_prac5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model_prac5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] newarray = { 4,6,7,788,8,8,8};
+            string[] qteam = new string[3];
+            qteam[0] = "TRD";
+            qteam[1] = "MW";
+            qteam[2] = "AO";
+            Console.WriteLine(newarray[3]);
+            Console.WriteLine(qteam[0]);
+            Console.ReadLine();
+        }
+    }
+}
