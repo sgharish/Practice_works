f32a4d6f32e5a1869aecfcba5d972fb39211511f
Harish S G <42273915+sgharish@users.noreply.github.com>
2019-11-29 11:42:59 +0100

    Add files via upload

---
+++ model_prac/Model_prac6/Model_prac6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model_prac6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            Console.Write("enter the number:");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("enter the another number:");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            calculator(num1,num2);
+            Console.ReadLine();
+
+        }
+
+
+        static void calculator(double a,double b)
+        {
+            double sum = a + b;
+            Console.WriteLine(sum);
+
+        }
+
+    }
+}
