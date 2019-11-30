5ba96b9acc66ebb7216e1c5c569ecff56291318c
Harish S G <42273915+sgharish@users.noreply.github.com>
2019-11-30 16:29:35 +0100

    Add files via upload

---
+++ model_prac/model_prac9/model_prac9/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model_prac9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetDay(10));
+            Console.ReadLine();
+        }
+        static string GetDay(int daynum)
+        {
+            string dayname;
+            switch (daynum )
+            {
+                case 0:
+                    dayname = "sunday";
+                    break;
+
+                case 1:
+                    dayname = "monday";
+                    break;
+
+                case 2:
+                    dayname = "Tuesday";
+                    break;
+
+
+                case 3:
+                    dayname = "Wednesday";
+                    break;
+
+
+                case 4:
+                    dayname = "thursday";
+                    break;
+
+
+                case 5:
+                    dayname = "Friday";
+                    break;
+
+
+                case 6:
+                    dayname = "Saturday";
+                    break;
+
+                default:
+                    dayname = " not a day in earth";
+                    break;
+
+            }
+            return dayname;
+
+        }
+    }
+}
