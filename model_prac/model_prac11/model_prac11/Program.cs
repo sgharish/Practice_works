08b3de9e0d9a954b9e9b71ef3447f8c0a88ac6df
Harish S G <42273915+sgharish@users.noreply.github.com>
2019-12-01 12:01:45 +0100

    Harry laptop(For loop)
    This is a mere copy of the example form my learning tutorial.

---
+++ model_prac/model_prac11/model_prac11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model_prac11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] number = { 4,5,6,66,6,6,6};
+            for (int i =0;i<number.Length;i++)
+            {
+                Console.WriteLine(number[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
