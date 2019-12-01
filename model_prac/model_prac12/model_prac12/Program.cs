9ca4699bad165433de7177f76db574d4885f9d1b
Harish S G <42273915+sgharish@users.noreply.github.com>
2019-12-01 14:15:52 +0100

    Harry laptop(reference)
    power function.

---
+++ model_prac/model_prac12/model_prac12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model_prac12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPow(2, 5));
+            Console.ReadLine();
+        }
+        static int GetPow(int bnum,int pnum)
+        {
+            int result = 1;
+            for (int i =0; i<pnum;i++ )
+            {
+                result = result * bnum;
+            }
+
+            return result;
+        }
+    }
+}
