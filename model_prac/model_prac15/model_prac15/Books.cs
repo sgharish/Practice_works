f994432838f6334fc258a2f8bc4eb2e0e8031652
Harish S G <42273915+sgharish@users.noreply.github.com>
2019-12-03 09:12:56 +0100

    Add files via upload

---
+++ model_prac/model_prac15/model_prac15/Books.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model_prac15
+{
+    class Books
+    {
+        string name, author, gener;
+        public Books(string bname, string bauthor, string bgener)
+        {
+            name = bname;
+            author = bauthor;
+            gener = bgener;
+        }
+        public void suggestion()
+        {
+            if (gener == "medicen")
+            {
+                Console.WriteLine("suggested for medicen students");
+            }
+            else if(gener == "engineering")
+            {
+                Console.WriteLine("suggested for engineering students");
+            }
+            else
+            {
+                Console.WriteLine("suggested for all students");
+            }
+        }
+        }
+    }
+
---
+++ model_prac/model_prac15/model_prac15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model_prac15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Books book1 = new Books("Art of physics in engineering", "Harish", "engineering");
+            Books book2 = new Books("Medicen for life", "Harish", "Medicen");
+            Books book3 = new Books("Engineering is more than anything", "Harish", "engineering");
+            Books book4 = new Books("Quantam physics", "Harish", "science");
+            Books book5 = new Books("Differential kienamatics", "Harish", "engineering");
+
+            book4.suggestion();
+            Console.ReadLine();
+        }
+    }
+}
